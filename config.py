98dc4407b2213be9cc56bbbd0cc4d64ea77a6a51
Robert Khousadian <31484519+rkhous@users.noreply.github.com>
2018-03-26 22:49:20 -0700

    add config

---
+++ config.py
@@ -0,0 +1,10 @@
+bot_channel = '123456789012345678' #limit users to using the bot in this channel, right click and copy ID.
+token= 'FjsjncOI28998-987d9fnxacFISANFDx' #discord bot token (NOT bot ID)
+website = 'http://mapnamehere.com' #URL to map if you want the .map function to work, simply gives the users the website
+
+
+#####DATABASE, limited to MySQL and MariaDB#####
+host = 'localhost'  #host
+user = 'root'  #database user
+password = 'password'  #database password
+database = 'Hydro'  #database name
