e626452588eb9409a6d6a31826faa07edc0b3174
ratersnake27 <35665976+ratersnake27@users.noreply.github.com>
2018-03-29 02:28:40 +0530

    Update cspm.py

--- cspm.py
+++ cspm.py
@@ -34,33 +34,55 @@ def find_pokemon_id(name):
 #raid function
 @bot.command(pass_context=True)
 async def raid(ctx, arg, arg2, arg3, arg4):#arg = gym name, arg2 = pokemon name, arg3 = level, arg4 = time remaining
-    if ctx and ctx.message.channel.id == str(bot_channel) and str(arg2).lower() in pokemon:
+    if ctx and ctx.message.channel.id == str(bot_channel):
         pokemon_id = find_pokemon_id(str(arg2).capitalize())
         ts = int(time.time())
-        time_battle = ts + int(arg4) * 60
-        time_spawn = time_battle - 3600
-        time_end = time_battle + 2760
-        try:
-            cursor.execute("SELECT id FROM forts WHERE NAME LIKE '%" + str(arg) + "%';")
-            gym_id = str(cursor.fetchall())
-            gym_id = gym_id.split(',')
-            gym_id = gym_id[0].split('((')
-            cursor.execute("INSERT INTO raids("
-                           "id, external_id, fort_id , level, "
-                           "pokemon_id, move_1, move_2, time_spawn, "
-                           "time_battle, time_end, cp)"
-                           "VALUES "
-                           "(null, null, " + str(gym_id[1]) + ", "
-                           + str(arg3) + ", " + str(pokemon_id) + ", "
-                           " null, null," + str(time_spawn) + ", " + str(time_battle) + ", " + str(time_end) + ", null);")
-            database.commit()
-            await bot.say('Successfully added your raid to the live map.')
-            await bot.send_message(discord.Object(id=log_channel), str(ctx.message.author.name) + ' said there was a ' + str(arg2) +
-                                   ' raid going on at ' + str(arg)) and print(str(ctx.message.author.name) + ' said there was a ' + str(arg2) +
-                                   ' raid going on at ' + str(arg))
-        except:
-            database.rollback()
-            await bot.say('Unsuccesful in database query, your raid was not added to the live map.')
+        if pokemon_id == "0":
+          time_battle = ts + int(arg4) * 60
+          time_spawn = time_battle - 3600
+          time_end = time_battle + 2760
+        else:
+          time_end = ts + int(arg4) * 60
+          time_battle = time_end - 2760
+          time_spawn = time_end - 6360
+          try:
+              if arg.isnumeric():
+                 gym_id = str(arg)
+                 cursor.execute("INSERT INTO raids("
+                                "id, external_id, fort_id , level, "
+                                "pokemon_id, move_1, move_2, time_spawn, "
+                                "time_battle, time_end, cp)"
+                                "VALUES "
+                                "(null, null, " + str(gym_id[1]) + ", "
+                                + str(arg3) + ", " + str(pokemon_id) + ", "
+                                " null, null," + str(time_spawn) + ", " + str(time_battle) + ", " + str(time_end) + ", null);")
+                 database.commit()
+                 await bot.say('Successfully added your raid to the live map.')
+                 await bot.send_message(discord.Object(id=log_channel), str(ctx.message.author.name) + ' said there was a ' + str(arg2) +
+                                        ' raid going on at ' + str(arg)) and print(str(ctx.message.author.name) + ' said there was a ' + str(arg2) +
+                                        ' raid going on at ' + str(arg))
+              else:
+                  cursor.execute("SELECT id FROM forts WHERE NAME LIKE '%" + str(arg) + "%';")
+                  gym_id = str(cursor.fetchall())
+                  gym_id = gym_id.split(',')
+                  gym_id = gym_id[0].split('((')
+                  cursor.execute("INSERT INTO raids("
+                                 "id, external_id, fort_id , level, "
+                                 "pokemon_id, move_1, move_2, time_spawn, "
+                                 "time_battle, time_end, cp)"
+                                 "VALUES "
+                                 "(null, null, " + str(gym_id[1]) + ", "
+                                 + str(arg3) + ", " + str(pokemon_id) + ", "
+                                 " null, null," + str(time_spawn) + ", " + str(time_battle) + ", " + str(time_end) + ", null);")
+                  database.commit()
+                  await bot.say('Successfully added your raid to the live map.')
+                  await bot.send_message(discord.Object(id=log_channel), str(ctx.message.author.name) + ' said there was a ' + str(arg2) +
+                                         ' raid going on at ' + str(arg)) and print(str(ctx.message.author.name) + ' said there was a ' + str(arg2) +
+                                         ' raid going on at ' + str(arg))
+          except:
+              database.rollback()
+              await bot.say('Unsuccesful in database query, your raid was not added to the live map.')
+
 @bot.command(pass_context=True)
 async def spawn(ctx, arg, arg2, arg3):
     if ctx and ctx.message.channel.id == str(bot_channel) and arg in pokemon:
@@ -86,6 +108,6 @@ async def spawn(ctx, arg, arg2, arg3):
 @bot.command(pass_context=True)
 async def map(ctx):
     if ctx:
-        await bot.say('Hey! Visit ' + str(website) + ' to see our crowd-sourced raids and spawns!')
+        await bot.say('Hey! Visit' + str(website) + ' to see our crowd-sourced spawns!')
 
 bot.run(token)
