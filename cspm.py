6f87b960082fcb3170e813d80213e8f4975392ab
ratersnake27 <35665976+ratersnake27@users.noreply.github.com>
2018-03-28 21:56:46 +0530

    Eggs Support

--- cspm.py
+++ cspm.py
@@ -4,8 +4,7 @@ from discord.ext import commands
 import asyncio
 from pokemonlist import pokemon, pokejson
 from config import bot_channel, token, host, user, password, database, website, log_channel
-import datetime
-import calendar
+import time
 
 bot = commands.Bot(command_prefix = '.')#set prefix to .
 
@@ -32,16 +31,15 @@ def find_pokemon_id(name):
                 return int(k)
         return 0
 
-def get_time(minute):
-    future = datetime.datetime.utcnow() + datetime.timedelta(minutes=minute)
-    return calendar.timegm(future.timetuple())
-
 #raid function
 @bot.command(pass_context=True)
 async def raid(ctx, arg, arg2, arg3, arg4):#arg = gym name, arg2 = pokemon name, arg3 = level, arg4 = time remaining
     if ctx and ctx.message.channel.id == str(bot_channel) and str(arg2).lower() in pokemon:
         pokemon_id = find_pokemon_id(str(arg2).capitalize())
-        time = get_time(int(arg4))
+        ts = int(time.time())
+        time_battle = ts + int(arg4) * 60
+        time_spawn = time_battle - 3600
+        time_end = time_battle + 2760
         try:
             cursor.execute("SELECT id FROM forts WHERE NAME LIKE '" + str(arg) + "%';")
             gym_id = str(cursor.fetchall())
@@ -53,8 +51,8 @@ async def raid(ctx, arg, arg2, arg3, arg4):#arg = gym name, arg2 = pokemon name,
                            "time_battle, time_end, cp)"
                            "VALUES "
                            "(null, null, " + str(gym_id[1]) + ", "
-                           + str(arg3) + ", " + str(pokemon_id) + ", null, null, "
-                           "null, null, " + str(time) + ", null);")
+                           + str(arg3) + ", " + str(pokemon_id) + ", "
+                           " null, null," + str(time_spawn) + ", " + str(time_battle) + ", " + str(time_end) + ", null);")
             database.commit()
             await bot.say('Successfully added your raid to the live map.')
             await bot.send_message(discord.Object(id=log_channel), str(ctx.message.author.name) + ' said there was a ' + str(arg2) +
@@ -67,13 +65,14 @@ async def raid(ctx, arg, arg2, arg3, arg4):#arg = gym name, arg2 = pokemon name,
 async def spawn(ctx, arg, arg2, arg3):
     if ctx and ctx.message.channel.id == str(bot_channel) and arg in pokemon:
         pokemon_id = find_pokemon_id(str(arg).capitalize())
-        time = get_time(15)
+        ts = int(time.time())
+        spawn_time = ts + 900
         try:
             cursor.execute("INSERT INTO sightings("
                            "id, pokemon_id, spawn_id, expire_timestamp, encounter_id, lat, lon, "
                            "atk_iv, def_iv, sta_iv, move_1, move_2, gender, "
                            "form, cp, level, updated, weather_boosted_condition, weather_cell_id, weight) "
-                           "VALUES (null, " + str(pokemon_id) +", null," + str(time) + ", null," + str(arg2) + ", " + str(arg3) +
+                           "VALUES (null, " + str(pokemon_id) +", null," + str(spawn_time) + ", null," + str(arg2) + ", " + str(arg3) +
                            ", null, null, null, null, null, null,"
                            " null, null, null, null, null, null, null);")
             database.commit()
