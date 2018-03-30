bb7f683f857219b5d027a6638261636f9a34f9ad
Robert Khousadian <rkhousadian@gmail.com>
2018-03-30 14:16:52 -0700

    add raids_channel for future patch

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ bot_channel = '123456789012345678' #limit users to using the bot in this channel
 token= 'FjsjncOI28998-987d9fnxacFISANFDx' #discord bot token (NOT bot ID)
 website = 'http://mapnamehere.com' #URL to map if you want the .map function to work, simply gives the users the website
 log_channel = '123456789012345678' #Add a log channel, copy the ID here, give this bot access to the channel.
+raids_channel = '123456789012345678' #create a channel for raids, copy ID here, give the bot access to writing in the channel.
 
 
 #####DATABASE, limited to MySQL and MariaDB#####
