a2bd777dda83f9bb8e2fa1cfe72b12af5fea3c54
Robert Khousadian <rkhousadian@gmail.com>
2018-04-02 21:28:34 -0700

    update config for spawn notifications

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ token= 'FjsjncOI28998-987d9fnxacFISANFDx' #discord bot token (NOT bot ID)
 website = 'http://mapnamehere.com' #URL to map if you want the .map function to work, simply gives the users the website
 log_channel = '123456789012345678' #Add a log channel, copy the ID here, give this bot access to the channel.
 raids_channel = '123456789012345678' #create a channel for raids, copy ID here, give the bot access to writing in the channel.
+spawn_channel = '123456789012345678' #create a channel for spawns, copy ID here, give the bot access to writing in the channel.
 
 
 #####DATABASE, limited to MySQL and MariaDB#####
